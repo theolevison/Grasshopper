453b4d3b7e5e8305e0d05841fbe8cad1d0c62e5f
ps1g19 <ps1g19@soton.ac.uk>
2022-01-07 14:29:44 +0200

    added a MainMenu

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -9,10 +9,13 @@ public class ItemSlot : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData){
         //make dropped item snap into position
         if (eventData.pointerDrag != null && enableSlot){
-            //set parent of dice to a dice slot
-            eventData.pointerDrag.GetComponent<RectTransform>().SetParent(GetComponent<RectTransform>());
-            eventData.pointerDrag.GetComponent<DragDrop>().slotChange = true;
-            //TODO: make sure only one dice can be put in each slot
+            //check to make sure only one dice can be put in each slot
+            if (this.transform.childCount == 0)
+            {
+                //set parent of dice to a dice slot
+                eventData.pointerDrag.GetComponent<RectTransform>().SetParent(GetComponent<RectTransform>());
+                eventData.pointerDrag.GetComponent<DragDrop>().slotChange = true;
+            }
         }
     }
 }
