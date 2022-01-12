3a2f505438d55db8faa1e1cc5109f136ab9d7f39
Theo Levison <theo.levison@gmail.com>
2022-01-12 18:09:52 +0000

    I have a big brain

--- Assets/Scripts/DebugController.cs
+++ Assets/Scripts/DebugController.cs
@@ -9,6 +9,7 @@ public class DebugController : MonoBehaviour
     private string input;
     public List<object> commandList;
     public static DebugCommand RELOAD_TASKS;
+    public static DebugCommand<string> SET_STAT; 
 
     public void OnToggleDebug(InputValue value){
         showConsole = !showConsole;
@@ -27,8 +28,16 @@ public class DebugController : MonoBehaviour
             Controller.Instance.ReloadTasks();
         });
 
+        SET_STAT = new DebugCommand<string>("set_stat", "sets a stat to the provided value, stat=value", "set_stat", (x) => {
+            string stat = x.Split('=')[0];
+            int value = int.Parse(x.Split('=')[1]);
+            Controller.Instance.stats[stat] = value;
+            Debug.Log("changed " + stat + " to " + value);
+        });
+
         commandList = new List<object>{
             RELOAD_TASKS,
+            SET_STAT
         };
     }
 
@@ -45,6 +54,8 @@ public class DebugController : MonoBehaviour
     }
 
     public void HandleInput(){
+        string[] properties = input.Split(' ');
+
         for (int i = 0; i < commandList.Count; i++)
         {
             DebugCommandBase commandBase = commandList[i] as DebugCommandBase;
@@ -52,6 +63,8 @@ public class DebugController : MonoBehaviour
             if (input.Contains(commandBase.commandId)){
                 if (commandList[i] as DebugCommand != null){
                     (commandList[i] as DebugCommand).Invoke();
+                } else if (commandList[i] as DebugCommand<string> != null){
+                    (commandList[i] as DebugCommand<string>).Invoke(properties[1]);
                 }
             }
         }
